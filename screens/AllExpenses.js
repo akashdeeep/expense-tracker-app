0f5ef98c9ec47a1651a30ca08508e89ab889ecff
akashdeeep <akashdeep@iitbhilai.ac.in>
2023-06-16 09:54:02 +0530

    finished expenses output

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -3,5 +3,5 @@ import { Text, View } from "react-native";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 export default AllExpenses = (props) => {
-	return <ExpensesOutput periodName="All" expenses={props.expenses} />;
+	return <ExpensesOutput expensePeriod="All" expenses={props.expenses} />;
 };
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -3,5 +3,7 @@ import { View, Text } from "react-native";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 export default RecentExpenses = (props) => {
-	return <ExpensesOutput periodName="Last 7 Days" expenses={props.expenses} />;
+	return (
+		<ExpensesOutput expensePeriod="Last 7 Days" expenses={props.expenses} />
+	);
 };
