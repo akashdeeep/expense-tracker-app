9fe81b3399b345fc2677c5ebe59f8e8c1bfc2493
akashdeeep <akashdeep@iitbhilai.ac.in>
2023-06-20 12:24:52 +0530

    works great till now.

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -3,15 +3,67 @@ import { View, StyleSheet, TextInput, Text } from "react-native";
 import { GlobalStyles } from "../../constants/GlobalStyles";
 
 function Input(props) {
-	let inputStyles = [styles.input];
-	if (props.textInputConfig && props.textInputConfig.multiline) {
-		inputStyles.push(styles.multilineInput);
+	const [text, setText] = useState("");
+	const [isFocused, setIsFocused] = useState(false);
+	const [isBlurred, setIsBlurred] = useState(false);
+	const [isTouched, setIsTouched] = useState(false);
+	const [isDirty, setIsDirty] = useState(false);
+	const [isValid, setIsValid] = useState(false);
+
+	const inputStyles = [styles2.input];
+	if (isFocused) {
+		inputStyles.push({ borderBottomColor: GlobalStyles.colors.primary100 });
 	}
-	console.log(props.style, styles2);
+	if (isBlurred) {
+		inputStyles.push({ borderBottomColor: GlobalStyles.colors.primary50 });
+	}
+	if (isTouched && !isDirty) {
+		inputStyles.push({ borderBottomColor: GlobalStyles.colors.primary50 });
+	}
+	if (isDirty && !isValid) {
+		inputStyles.push({ borderBottomColor: GlobalStyles.colors.error });
+	}
+
+	const textChangeHandler = (text) => {
+		setText(text);
+	};
+
+	const focusHandler = () => {
+		setIsFocused(true);
+		setIsBlurred(false);
+	};
+
+	const blurHandler = () => {
+		setIsFocused(false);
+		setIsBlurred(true);
+	};
+
+	const touchHandler = () => {
+		setIsTouched(true);
+	};
+
+	const dirtyHandler = () => {
+		setIsDirty(true);
+	};
+
+	const validHandler = () => {
+		setIsValid(true);
+	};
+
 	return (
 		<View style={[styles2.inputContainer, props.style]}>
 			<Text style={styles2.label}>{props.label}</Text>
-			<TextInput style={styles2.input} {...props.textInputConfig} />
+			<TextInput
+				{...props.textInputConfig}
+				style={inputStyles}
+				value={text}
+				onChangeText={textChangeHandler}
+				onFocus={focusHandler}
+				onBlur={blurHandler}
+				onTouchStart={touchHandler}
+				onTouchEnd={dirtyHandler}
+				onEndEditing={validHandler}
+			/>
 		</View>
 	);
 }
